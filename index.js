007fe8795fda50c257d6837c8ad1dc6ad59024ad
talhaworkspaceio <talha.workspace.io@gmail.com>
2026-02-01 01:02:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -217,7 +217,7 @@ eventsRef.on('child_added', async snap => {
     tokens,
     notification: { title, body },
     android: {
-      notification: { channelId: 'default', smallIcon: 'ic_notification',},
+      notification: { channelId: 'default', smallIcon: 'ic_notification'},
     },
     apns: {
       payload: {
